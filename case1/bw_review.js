1c1075ff4c69b39d5b9ae792afb3b9098050f33a
Ariahnnah <Aporras177@west-mec.org>
2020-03-12 15:25:10 -0700

    Finished Project 1
    Finished Project 1

--- case1/bw_review.js
+++ case1/bw_review.js
@@ -44,31 +44,54 @@ function init() {
 }
 
 function lightStars(event) {
-   var stars = document.querySelectorAll("span#stars img");
-   var target = event.target;
-   target.addEventListener("mouseleave", function (event) {
-      console.log("leaving", event.target.getAttribute("alt"));
-      var leavingStarNumber = event.target.getAttribute("alt")
-      for (var i = leavingStarNumber; i < stars.length; i++) {
-         stars[i].src = "bw_star.png";
-         console.log(stars)
-      }
-   })
-   var starNumber = event.target.getAttribute("alt");
-   console.log("test", starNumber);
+   var stars = document.querySelectorAll("span#stars img"); //stores location of each star img
+   var starNumber = event.target.getAttribute("alt"); //targets the alt attribute of each star
 
+   //makes the star checked and shows how many are selected in the DOM
    for (var i = 0; i < starNumber; i++) {
       stars[i].src = "bw_star2.png";
       document.querySelector("input#rating").value = starNumber + " stars";
    }
+
+   //sets the star back to unselected after the mouse is moved away from it
+ //makes the stars stay if you click it
    for (var i = starNumber; i < 5; i++) {
       stars[i].src = "bw_star.png";
-
-      console.log("Exit", i);
+      stars[i].addEventListener("mouseleave", turnOffStars);
+      event.target.addEventListener("click", function (event) {
+         event.target.removeEventListener("mouseleave", turnOffStars);
+      })
    }
 }
 
-function updateCont() { }
+//used in conjunction with the for loop above
+function turnOffStars() {
+   var stars = document.querySelectorAll("span#stars img");
+
+   for (var i = starNumber; i < 5; i++) {
+      stars[i].src = "bw_star.png";
+      document.querySelector("input#rating").value = "";
+   }
+}
+
+//This code sets the char count as the user types and the if statement 
+// achanges the text to see if there is an error after 1000 characters
+function updateCont() {
+   var commentText = document.querySelector("textarea#comment").value;
+   var charCount = countCharacters(commentText); //calls the function below the divider which handles the char count
+   var wordCountBox = document.querySelector("input#wordCount");
+
+   wordCountBox.value = charCount + "/1000";
+
+   if(charCount > 1000){
+      wordCountBox.style.color = "white";
+      wordCountBox.style.backgroundColor = "red";
+   }
+   else{
+      wordCountBox.style.color = "black";
+      wordCountBox.style.backgroundColor = "white";
+   }
+}
 
 /*=================================================================*/
 
